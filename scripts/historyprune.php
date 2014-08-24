3dfe9c6dfab7969038233ddbed0dc77e6bfc3958
Erorus <github@theunderminejournal.com>
2014-08-24 11:50:49 -0400

    history prune allows 24*14 snapshots or 2 weeks, whichever is older

--- scripts/historyprune.php
+++ scripts/historyprune.php
@@ -27,7 +27,7 @@ function CleanOldData()
     $houses = array_values(DBMapArray($result));
     $stmt->close();
 
-    $sqlPattern = 'delete from tblItemHistory where house = %d and item between %d and %d and snapshot < timestampadd(day, %d, now())';
+    $sqlPattern = 'delete from tblItemHistory where house = %d and item between %d and %d and snapshot < \'%s\'';
 
     for ($hx = 0; $hx < count($houses); $hx++)
     {
@@ -36,6 +36,25 @@ function CleanOldData()
             return;
 
         $house = $houses[$hx];
+        $ssDate = '';
+        $cutoffDate = Date('Y-m-d H:i:s', strtotime(''.HISTORY_DAYS.' days ago'));
+
+        $stmt = $db->prepare('select min(`updated`) from (select `updated` from tblSnapshot where house = ? order by updated desc limit ?) aa');
+        $maxSnapshots = 24 * HISTORY_DAYS;
+        $stmt->bind_param('ii', $house, $maxSnapshots);
+        $stmt->execute();
+        $stmt->bind_result($ssDate);
+        $gotDate = $stmt->fetch() === true;
+        $stmt->close();
+
+        if (!$gotDate || is_null($ssDate))
+        {
+            DebugMessage("$house has no snapshots, skipping!");
+            continue;
+        }
+
+        if (strtotime($ssDate) < strtotime($cutoffDate))
+            $cutoffDate = $ssDate;
 
         foreach (array($house, -1 * $house) as $factionHouse)
         {
@@ -43,9 +62,8 @@ function CleanOldData()
             if ($caughtKill)
                 return;
 
-            $stmt = $db->prepare('select distinct item from tblItemSummary where house = ? and lastseen > timestampadd(day, ?, now())');
-            $days = -1 * (HISTORY_DAYS + 1);
-            $stmt->bind_param('ii',$factionHouse,$days);
+            $stmt = $db->prepare('select distinct item from tblItemSummary where house = ? and lastseen > timestampadd(day, -1, ?)');
+            $stmt->bind_param('is',$factionHouse,$cutoffDate);
             $stmt->execute();
             $result = $stmt->get_result();
             $items = array_values(DBMapArray($result));
@@ -55,9 +73,9 @@ function CleanOldData()
 
             if (count($items) > 0)
             {
-                $db->real_query(sprintf(str_replace('item between %d and %d', 'item < %d', $sqlPattern), $factionHouse, $items[0], -1 * HISTORY_DAYS));
+                $db->real_query(sprintf(str_replace('item between %d and %d', 'item < %d', $sqlPattern), $factionHouse, $items[0], $cutoffDate));
                 $rowCount += $db->affected_rows;
-                $db->real_query(sprintf(str_replace('item between %d and %d', 'item > %d', $sqlPattern), $factionHouse, $items[count($items) - 1], -1 * HISTORY_DAYS));
+                $db->real_query(sprintf(str_replace('item between %d and %d', 'item > %d', $sqlPattern), $factionHouse, $items[count($items) - 1], $cutoffDate));
                 $rowCount += $db->affected_rows;
 
                 $itemChunks = array_chunk($items, 100);
@@ -67,22 +85,22 @@ function CleanOldData()
                     heartbeat();
                     $minItem = array_shift($itemChunks[$x]);
                     $maxItem = count($itemChunks[$x]) > 0 ? array_pop($itemChunks[$x]) : $minItem;
-                    $db->real_query(sprintf($sqlPattern, $factionHouse, $minItem, $maxItem, -1 * HISTORY_DAYS));
+                    $db->real_query(sprintf($sqlPattern, $factionHouse, $minItem, $maxItem, $cutoffDate));
                     $rowCount += $db->affected_rows;
                 }
 
-                $db->real_query(sprintf(str_replace(' and item between %d and %d', '', $sqlPattern), $factionHouse, -1 * HISTORY_DAYS));
+                $db->real_query(sprintf(str_replace(' and item between %d and %d', '', $sqlPattern), $factionHouse, $cutoffDate));
                 $rowCount += $db->affected_rows;
             }
 
-            DebugMessage("$rowCount item history rows deleted from house $factionHouse");
+            DebugMessage("$rowCount item history rows deleted from house $factionHouse since $cutoffDate");
         }
     }
 
     if ($caughtKill)
         return;
 
-    $sqlPattern = 'delete from tblPetHistory where house = %d and species between %d and %d and snapshot < timestampadd(day, %d, now())';
+    $sqlPattern = 'delete from tblPetHistory where house = %d and species between %d and %d and snapshot < \'%s\'';
 
     for ($hx = 0; $hx < count($houses); $hx++)
     {
@@ -91,6 +109,25 @@ function CleanOldData()
             return;
 
         $house = $houses[$hx];
+        $ssDate = '';
+        $cutoffDate = Date('Y-m-d H:i:s', strtotime(''.HISTORY_DAYS.' days ago'));
+
+        $stmt = $db->prepare('select min(`updated`) from (select `updated` from tblSnapshot where house = ? order by updated desc limit ?) aa');
+        $maxSnapshots = 24 * HISTORY_DAYS;
+        $stmt->bind_param('ii', $house, $maxSnapshots);
+        $stmt->execute();
+        $stmt->bind_result($ssDate);
+        $gotDate = $stmt->fetch() === true;
+        $stmt->close();
+
+        if (!$gotDate || is_null($ssDate))
+        {
+            DebugMessage("$house has no snapshots, skipping!");
+            continue;
+        }
+
+        if (strtotime($ssDate) < strtotime($cutoffDate))
+            $cutoffDate = $ssDate;
 
         foreach (array($house, -1 * $house) as $factionHouse)
         {
@@ -98,9 +135,8 @@ function CleanOldData()
             if ($caughtKill)
                 return;
 
-            $stmt = $db->prepare('select distinct species from tblPetSummary where house = ? and lastseen > timestampadd(day, ?, now())');
-            $days = -1 * (HISTORY_DAYS + 1);
-            $stmt->bind_param('ii',$factionHouse,$days);
+            $stmt = $db->prepare('select distinct species from tblPetSummary where house = ? and lastseen > timestampadd(day, -1, ?)');
+            $stmt->bind_param('is',$factionHouse,$cutoffDate);
             $stmt->execute();
             $result = $stmt->get_result();
             $items = array_values(DBMapArray($result));
@@ -110,9 +146,9 @@ function CleanOldData()
 
             if (count($items) > 0)
             {
-                $db->real_query(sprintf(str_replace('species between %d and %d', 'species < %d', $sqlPattern), $factionHouse, $items[0], -1 * HISTORY_DAYS));
+                $db->real_query(sprintf(str_replace('species between %d and %d', 'species < %d', $sqlPattern), $factionHouse, $items[0], $cutoffDate));
                 $rowCount += $db->affected_rows;
-                $db->real_query(sprintf(str_replace('species between %d and %d', 'species > %d', $sqlPattern), $factionHouse, $items[count($items) - 1], -1 * HISTORY_DAYS));
+                $db->real_query(sprintf(str_replace('species between %d and %d', 'species > %d', $sqlPattern), $factionHouse, $items[count($items) - 1], $cutoffDate));
                 $rowCount += $db->affected_rows;
 
                 $itemChunks = array_chunk($items, 100);
@@ -122,15 +158,15 @@ function CleanOldData()
                     heartbeat();
                     $minItem = array_shift($itemChunks[$x]);
                     $maxItem = count($itemChunks[$x]) > 0 ? array_pop($itemChunks[$x]) : $minItem;
-                    $db->real_query(sprintf($sqlPattern, $factionHouse, $minItem, $maxItem, -1 * HISTORY_DAYS));
+                    $db->real_query(sprintf($sqlPattern, $factionHouse, $minItem, $maxItem, $cutoffDate));
                     $rowCount += $db->affected_rows;
                 }
 
-                $db->real_query(sprintf(str_replace(' and species between %d and %d', '', $sqlPattern), $factionHouse, -1 * HISTORY_DAYS));
+                $db->real_query(sprintf(str_replace(' and species between %d and %d', '', $sqlPattern), $factionHouse, $cutoffDate));
                 $rowCount += $db->affected_rows;
             }
 
-            DebugMessage("$rowCount pet history rows deleted from house $factionHouse");
+            DebugMessage("$rowCount pet history rows deleted from house $factionHouse since $cutoffDate");
         }
     }
 
