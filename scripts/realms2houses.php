7d5d01fc63af84c957e2158c52eafa11cbd07bbd
Erorus <github@theunderminejournal.com>
2014-08-26 03:42:13 +0000

    realms2houses checking data and setting canonical slugs

--- scripts/realms2houses.php
+++ scripts/realms2houses.php
@@ -55,12 +55,95 @@ foreach ($regions as $region)
     }
     $stmt->close();
 
-    $stmt = $db->prepare('select slug, house from tblRealm where region = ?');
+    $stmt = $db->prepare('select slug, house, name from tblRealm where region = ?');
     $stmt->bind_param('s', $region);
     $stmt->execute();
     $result = $stmt->get_result();
-    $houses = DBMapArray($result);
+    $bySlug = DBMapArray($result);
     $stmt->close();
+
+    $canonicals = array();
+    $bySellerRealm = array();
+    $fallBack = array();
+    $candidates = array();
+    $winners = array();
+
+    foreach ($bySlug as $row)
+    {
+        heartbeat();
+        if ($caughtKill)
+            break 2;
+
+        $slug = $row['slug'];
+        $bySellerRealm[str_replace(' ', '', $row['name'])] = $row['slug'];
+
+        DebugMessage("Fetching $region $slug");
+        $url = sprintf('https://%s.battle.net/api/wow/auction/data/%s', strtolower($region), $slug);
+
+        $json = FetchHTTP($url, array('noFetchLimit' => true));
+        $dta = json_decode($json, true);
+        if (!isset($dta['files']))
+        {
+            DebugMessage("$region $slug returned no files.", E_USER_WARNING);
+            continue;
+        }
+
+        $hash = preg_match('/\b[a-f0-9]{32}\b/', $dta['files'][0]['url'], $res) > 0 ? $res[0] : '';
+        if ($hash == '')
+        {
+            DebugMessage("$region $slug had no hash in the URL: {$dta['files'][0]['url']}", E_USER_WARNING);
+            continue;
+        }
+
+        $a = GetDataRealms($region, $hash);
+        if ($a['slug'])
+        {
+            $canonicals[$a['slug']][md5(json_encode($a))] = $a;
+            $fallBack[$slug] = $a['slug'];
+        }
+    }
+
+    foreach ($canonicals as $canon => $results)
+    {
+        if (count($results) > 1)
+            DebugMessage("$region $canon has ".count($results)." results: ".print_r($results, true));
+
+        foreach ($results as $result)
+            foreach ($result['realms'] as $sellerRealm)
+                if (isset($bySellerRealm[$sellerRealm]))
+                    $candidates[$bySellerRealm[$sellerRealm]][$canon] = count($result['realms']) + (isset($candidates[$bySellerRealm[$sellerRealm]][$canon]) ? $candidates[$bySellerRealm[$sellerRealm]][$canon] : 0);
+    }
+
+    $byCanonical = array();
+    foreach ($bySlug as $row)
+    {
+        if (isset($candidates[$row['slug']]))
+        {
+            arsort($candidates[$row['slug']]);
+            $c2 = array();
+            $c2cnt = 0;
+            foreach ($candidates[$row['slug']] as $canon => $cnt)
+            {
+                if ($c2cnt == 0)
+                {
+                    $c2cnt = $cnt;
+                    $c2[] = $canon;
+                }
+                else if ($c2cnt == $cnt)
+                    $c2[] = $canon;
+            }
+            sort($c2);
+            $winners[$row['slug']] = $c2[0];
+        }
+        else
+            $winners[$row['slug']] = isset($fallBack[$row['slug']]) ? $fallBack[$row['slug']] : $row['slug'];
+
+        $byCanonical[$winners[$row['slug']]][] = $row['slug'];
+    }
+
+    heartbeat();
+    if ($caughtKill)
+        break;
 
     $stmt = $db->prepare('select ifnull(max(house),0) from tblRealm');
     $stmt->execute();
@@ -68,92 +151,22 @@ foreach ($regions as $region)
     $stmt->fetch();
     $stmt->close();
 
-    $hashes = array();
-    $retries = 0;
-
-    while ($retries++ < 10)
+    foreach ($byCanonical as $canon => $slugs)
     {
-        $started = time();
-        $needRetry = false;
-        $slugs = array_keys($houses);
-        foreach ($slugs as $slug)
-        {
-            heartbeat();
-            if ($caughtKill)
-                break 3;
-
-            if (isset($houses[$slug]['modified']))
-                continue;
-
-            //DebugMessage("Fetching $region $slug");
-            $url = sprintf('https://%s.battle.net/api/wow/auction/data/%s', strtolower($region), $slug);
-
-            $json = FetchHTTP($url, array('noFetchLimit' => true));
-            $dta = json_decode($json, true);
-            if (!isset($dta['files']))
-            {
-                DebugMessage("$region $slug returned no files.", E_USER_WARNING);
-                continue;
-            }
-
-            $hash = preg_match('/\b[a-f0-9]{32}\b/', $dta['files'][0]['url'], $res) > 0 ? $res[0] : '';
-            if ($hash == '')
-            {
-                DebugMessage("$region $slug had no hash in the URL: {$dta['files'][0]['url']}", E_USER_WARNING);
-                continue;
-            }
-
-            DebugMessage("$region $slug $hash");
-
-            $modified = floor(intval($dta['files'][0]['lastModified'], 10)/1000);
-            if ($modified > $started)
-            {
-                if (isset($hashes[$hash]))
-                {
-                    DebugMessage("$region $slug was updated $modified after we started $started, but we've already seen that hash. Continuing.");
-                }
-                else
-                {
-                    DebugMessage("$region $slug was updated $modified after we started $started. Starting over.");
-                    $k = array_keys($hashes);
-                    foreach ($k as $hash)
-                    {
-                        if ($houses[$hashes[$hash][0]]['modified'] < (time() - 20 * 60))
-                        {
-                            foreach ($hashes[$hash] as $wipeSlug)
-                                unset($houses[$wipeSlug]['hash'], $houses[$wipeSlug]['modified']);
-                            unset($hashes[$hash]);
-                        }
-                    }
-                    $needRetry = true;
-                    break;
-                }
-            }
-
-            $houses[$slug]['hash'] = $hash;
-            $houses[$slug]['modified'] = $modified;
-            if (!isset($hashes[$hash]))
-                $hashes[$hash] = array();
-
-            $hashes[$hash][] = $slug;
-        }
-        if (!$needRetry)
-            break;
-    }
-
-    heartbeat();
-    if ($caughtKill)
-        break;
-    foreach ($hashes as $hash => $slugs)
-    {
+        sort($slugs);
+        $rep = $slugs[0];
         $candidates = array();
         foreach ($slugs as $slug)
-            if (!is_null($houses[$slug]['house']))
+        {
+            if ($slug == $canon)
+                $rep = $slug;
+            if (!is_null($bySlug[$slug]['house']))
             {
-                if (!isset($candidates[$houses[$slug]['house']]))
-                    $candidates[$houses[$slug]['house']] = 0;
-                $candidates[$houses[$slug]['house']]++;
+                if (!isset($candidates[$bySlug[$slug]['house']]))
+                    $candidates[$bySlug[$slug]['house']] = 0;
+                $candidates[$bySlug[$slug]['house']]++;
             }
+        }
         if (count($candidates) > 0)
         {
             asort($candidates);
@@ -162,30 +175,93 @@ foreach ($regions as $region)
         }
         else
             $curHouse = ++$maxHouse;
-        $houseKeys = array_keys($houses);
+        $houseKeys = array_keys($bySlug);
         foreach ($houseKeys as $slug)
         {
             if (in_array($slug, $slugs))
                 continue;
-            if ($houses[$slug]['house'] == $curHouse)
-                $houses[$slug]['house'] = null;
+            if ($bySlug[$slug]['house'] == $curHouse)
+                $bySlug[$slug]['house'] = null;
         }
         foreach ($slugs as $slug)
         {
-            if ($houses[$slug]['house'] != $curHouse)
+            if ($bySlug[$slug]['house'] != $curHouse)
             {
-                DebugMessage("$region $slug changing from ".(is_null($houses[$slug]['house']) ? 'null' : $houses[$slug]['house'])." to $curHouse");
+                DebugMessage("$region $slug changing from ".(is_null($bySlug[$slug]['house']) ? 'null' : $bySlug[$slug]['house'])." to $curHouse");
                 $db->real_query(sprintf('update tblRealm set house = %d where region = \'%s\' and slug = \'%s\'', $curHouse, $db->escape_string($region), $db->escape_string($slug)));
-                $houses[$slug]['house'] = $curHouse;
+                $bySlug[$slug]['house'] = $curHouse;
             }
         }
+        $db->real_query(sprintf('update tblRealm set canonical = null where house = %d', $curHouse));
+        $db->real_query(sprintf('update tblRealm set canonical = \'%s\' where house = %d and region = \'%s\' and slug = \'%s\'', $db->escape_string($canon), $curHouse, $db->escape_string($region), $db->escape_string($rep)));
     }
     $memcache->delete('realms_'.$region);
 }
 
-CleanOldHouses();
+//CleanOldHouses();
+DebugMessage('Skipped cleaning old houses!');
 
 DebugMessage('Done!');
+
+function GetDataRealms($region, $hash)
+{
+    heartbeat();
+    $region = strtolower($region);
+
+    $pth = __DIR__.'/realms2houses_cache';
+    if (!is_dir($pth))
+        DebugMessage('Could not find realms2houses_cache!', E_USER_ERROR);
+
+    $cachePath = "$pth/$region-$hash.json";
+
+    if (file_exists($cachePath) && (filemtime($cachePath) > (time() - 23*60*60)))
+        return json_decode(file_get_contents($cachePath), true);
+
+    $result = array('slug' => false, 'realms' => array());
+
+    $url = sprintf('http://%s.battle.net/auction-data/%s/auctions.json', $region, $hash);
+    $outHeaders = array();
+    $json = FetchHTTP($url, [], $outHeaders);
+    if (!$json)
+    {
+        DebugMessage("No data from $url, waiting 5 secs");
+        http_persistent_handles_clean();
+        sleep(5);
+        $json = FetchHTTP($url, [], $outHeaders);
+    }
+
+    if (!$json)
+    {
+        DebugMessage("No data from $url, waiting 15 secs");
+        http_persistent_handles_clean();
+        sleep(15);
+        $json = FetchHTTP($url, [], $outHeaders);
+    }
+
+    if (!$json)
+    {
+        if (file_exists($cachePath) && (filemtime($cachePath) > (time() - 3*24*60*60)))
+        {
+            DebugMessage("No data from $url, using cache");
+            return json_decode(file_get_contents($cachePath), true);
+        }
+        DebugMessage("No data from $url, giving up");
+        return $result;
+    }
+
+    $xferBytes = isset($outHeaders['X-Original-Content-Length']) ? $outHeaders['X-Original-Content-Length'] : strlen($data);
+    DebugMessage("$region $hash data file ".strlen($json)." bytes".($xferBytes != strlen($json) ? (' (transfer length '.$xferBytes.', '.round($xferBytes/strlen($json)*100,1).'%)') : ''));
+
+    if (preg_match('/"slug":"([^"?]+)"/', $json, $m))
+        $result['slug'] = $m[1];
+
+    preg_match_all('/"ownerRealm":"([^"?]+)"/', $json, $m);
+    $result['realms'] = array_values(array_unique($m[1]));
+
+    file_put_contents($cachePath, json_encode($result));
+
+    return $result;
+}
 
 function CleanOldHouses()
 {
