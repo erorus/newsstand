61b493d70209da56e96140ce729ab443d3c3b490
Erorus <github@theunderminejournal.com>
2015-01-01 19:11:48 +0000

    remove cloudflare cookie

--- public/api/realms.php
+++ public/api/realms.php
@@ -6,6 +6,10 @@ require_once('../../incl/api.incl.php');
 
 header('Expires: ' . Date(DATE_RFC1123, strtotime('+3 hours')));
 
+if (isset($_COOKIE['__cfduid'])) { // cloudflare
+    setcookie('__cfduid', '', strtotime('1 year ago'), '/', '.theunderminejournal.com', false, true);
+}
+
 $region = GetSiteRegion();
 
 json_return(array('version' => API_VERSION, 'region' => $region, 'realms' => GetRealms($region)));
