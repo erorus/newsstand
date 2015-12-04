8a38d933dd290c4bc0bb524c33b9a2d85ebfec13
Erorus <github@theunderminejournal.com>
2015-12-04 16:33:28 -0500

    Revert "omit expensive "cheaper" column on sellers with many auctions up"
    This reverts commit 5df40c565139fa0d0ce0099f87524b301e0cacbe.

--- public/js/seller.js
+++ public/js/seller.js
@@ -423,13 +423,10 @@ var TUJ_Seller = function ()
         td.className = 'price';
         $(td).text('Buyout Each');
 
-        var hasCheaper = data.auctions.length && data.auctions[0].hasOwnProperty('cheaper');
-        if (hasCheaper) {
-            td = libtuj.ce('th');
-            tr.appendChild(td);
-            td.className = 'quantity';
-            $(td).text('Cheaper');
-        }
+        td = libtuj.ce('th');
+        tr.appendChild(td);
+        td.className = 'quantity';
+        $(td).text('Cheaper');
 
         data.auctions.sort(function (a, b)
         {
@@ -512,13 +509,11 @@ var TUJ_Seller = function ()
                 td.appendChild(a);
             }
 
-            if (hasCheaper) {
-                td = libtuj.ce('td');
-                tr.appendChild(td);
-                td.className = 'quantity';
-                if (auc.cheaper) {
-                    td.appendChild(libtuj.FormatQuantity(auc.cheaper));
-                }
+            td = libtuj.ce('td');
+            tr.appendChild(td);
+            td.className = 'quantity';
+            if (auc.cheaper) {
+                td.appendChild(libtuj.FormatQuantity(auc.cheaper));
             }
         }
 
