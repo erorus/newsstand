ae990ddd0dc3b9cd9c1610e5c940de04fb32f89e
Erorus <github@theunderminejournal.com>
2016-02-11 22:39:02 -0500

    use local mail server

--- public/api/contact.php
+++ public/api/contact.php
@@ -3,7 +3,6 @@
 require_once('../../incl/incl.php');
 require_once('../../incl/memcache.incl.php');
 require_once('../../incl/api.incl.php');
-require_once('../../incl/mail.credentials.php');
 
 $fields = array('from', 'subject', 'message');
 foreach ($fields as $field) {
@@ -18,14 +17,14 @@ if ($_POST['subject'] != "Subject") {
 
 unset($_POST['subject']);
 
-if (preg_match('/\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/i', $_POST['from'], $res) > 0) {
-    $mailCredentials['from'] = $res[0];
-}
-
 $headers = array();
 $headers['Date'] = Date(DATE_RFC2822);
 $headers['Content-Type'] = 'text/plain; charset=ISO-8859-1; format=flowed';
+$headers['From'] = 'Contact Form <contactform@from.theunderminejournal.com>';
 
+if (preg_match('/\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/i', $_POST['from'], $res) > 0) {
+    $headers['Reply-To'] = $res[0];
+}
 
 $_POST['message'] = preg_replace('/\r\n?/', "\n", $_POST['message']);
 
@@ -48,22 +47,13 @@ $body .= "\n---------------\n" . $_POST['message'];
 
 $body = preg_replace_callback('/(?<=^|\n)([^\n]+)(?:\n|$)/', create_function('$m', 'return ((strlen(trim($m[1]))>0)?wordwrap($m[1],66," \n"):"")."\n";'), strip_8bit_chars(utf8_decode(strip_tags(str_replace('&amp;', '&', str_replace('&quot;', '"', str_replace('&lt;', '<', $body)))))));
 
-$cmd = '/bin/mailx ';
-foreach ($mailCredentials as $k => $v) {
-    $cmd .= ' -S ' . escapeshellarg($k . '=' . $v);
+$headerString = '';
+foreach ($headers as $k => $v) {
+    $headerString .= ($headerString == '' ? '' : "\n") . "$k: $v";
 }
-$cmd .= ' -s ' . escapeshellarg('Letter to the Editor') . ' ' . escapeshellarg('contactform@theunderminejournal.com');
+$result = mail('The Editor <editor@theunderminejournal.com>','Letter to the Editor',$body,$headerString, '-fcontactform@from.theunderminejournal.com');
 
-$f = tempnam('/tmp', 'contactform');
-file_put_contents($f, $body);
-$cmd .= ' < ' . escapeshellarg($f);
-
-$output = array();
-$result = 0;
-exec($cmd, $output, $result);
-unlink($f);
-
-json_return($result === 0 ? array() : false);
+json_return($result ? array() : false);
 
 function strip_8bit_chars($str)
 {
