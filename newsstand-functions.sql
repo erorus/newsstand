bd29d17ab61c3531ad27dfe780a43c0652024ad0
Erorus <github@theunderminejournal.com>
2016-07-04 17:04:49 +0000

    better location for database functions

--- newsstand-functions.sql
+++ newsstand-functions.sql
@@ -16,14 +16,66 @@ CREATE FUNCTION GetMarketPrice(inHouse smallint, inID mediumint, inBonusSet tiny
     THEN RETURN tr; END IF;
 
     if inDate is not null then
-      select price
+      select (case hour(s.updated)
+              when 0 then ih.silver00
+              when 1 then ih.silver01
+              when 2 then ih.silver02
+              when 3 then ih.silver03
+              when 4 then ih.silver04
+              when 5 then ih.silver05
+              when 6 then ih.silver06
+              when 7 then ih.silver07
+              when 8 then ih.silver08
+              when 9 then ih.silver09
+              when 10 then ih.silver10
+              when 11 then ih.silver11
+              when 12 then ih.silver12
+              when 13 then ih.silver13
+              when 14 then ih.silver14
+              when 15 then ih.silver15
+              when 16 then ih.silver16
+              when 17 then ih.silver17
+              when 18 then ih.silver18
+              when 19 then ih.silver19
+              when 20 then ih.silver20
+              when 21 then ih.silver21
+              when 22 then ih.silver22
+              when 23 then ih.silver23
+              else null end) * 100
       into tr
-      from tblItemHistory
-      where house = inHouse
-      and item = inID
-      and bonusset = ifnull(inBonusSet, bonusset)
-      and `snapshot` <= inDate
-      order by `snapshot` desc, if(quantity=0, 0, 1) desc, price desc
+      from tblSnapshot s
+        join tblItemHistoryHourly ih on date(s.updated) = ih.`when` and ih.house = s.house
+      where s.house = inHouse
+            and ih.item = inID
+            and ih.bonusset = ifnull(inBonusSet, ih.bonusset)
+            and s.updated <= inDate
+            and ifnull(case hour(s.updated)
+                       when 0 then ih.quantity00
+                       when 1 then ih.quantity01
+                       when 2 then ih.quantity02
+                       when 3 then ih.quantity03
+                       when 4 then ih.quantity04
+                       when 5 then ih.quantity05
+                       when 6 then ih.quantity06
+                       when 7 then ih.quantity07
+                       when 8 then ih.quantity08
+                       when 9 then ih.quantity09
+                       when 10 then ih.quantity10
+                       when 11 then ih.quantity11
+                       when 12 then ih.quantity12
+                       when 13 then ih.quantity13
+                       when 14 then ih.quantity14
+                       when 15 then ih.quantity15
+                       when 16 then ih.quantity16
+                       when 17 then ih.quantity17
+                       when 18 then ih.quantity18
+                       when 19 then ih.quantity19
+                       when 20 then ih.quantity20
+                       when 21 then ih.quantity21
+                       when 22 then ih.quantity22
+                       when 23 then ih.quantity23
+                       else null end, 0) > 0
+      order by s.updated desc
       limit 1;
     end if;
 
@@ -92,7 +144,7 @@ CREATE PROCEDURE GetReagentPriceR(
       FROM tblDBCItemReagents
       WHERE item = inID;
 
-      IF (inLevels >= 4) OR (x = 0)
+      IF (inLevels >= 3) OR (x = 0)
       THEN
         LEAVE sproc;
       END IF;
