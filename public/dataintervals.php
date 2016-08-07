b78113f1e8a09ebc13b0eb28cbb7726dcd2e881e
Erorus <github@theunderminejournal.com>
2016-08-07 14:04:11 -0400

    update dataintervals query with delay mode

--- public/dataintervals.php
+++ public/dataintervals.php
@@ -1,14 +1,9 @@
 <?php
 
-if (strtotime('June 30, 2016') > time()) {
-    header('Location: https://does.theapi.work/');
-    exit;
-}
-
 require_once('../incl/incl.php');
 require_once('../incl/memcache.incl.php');
 
-$now = Date('r');
+$now = date('r');
 
 echo <<<EOF
 <!DOCTYPE html>
@@ -18,7 +13,7 @@ echo <<<EOF
 <body>
 <h1>Battle.net Auction House API Data Intervals</h1>
 
-Delays observed between AH API snapshots over the past 72 hours, as of $now
+Delays observed between AH API snapshots over the past 48 hours, as of $now
 <p>
 
 EOF;
@@ -29,27 +24,51 @@ echo '</body></html>';
 
 function DataIntervalsData()
 {
-    $cacheKey = 'dataintervalstable2';
+    $cacheKey = 'dataintervalstable';
 
     if (($tr = MCGet($cacheKey)) !== false)
         return $tr;
 
     $db = DBConnect();
 
-    $sql = <<<EOF
-select t.house, t.lastupdate, t.mindelta * 60 mindelta, t.avgdelta * 60 avgdelta, t.maxdelta * 60 maxdelta, r.region, group_concat(r.name order by 1 separator ', ') nms from (
-select deltas.house, max(deltas.updated) lastupdate, round(min(delta)/60) mindelta, round(avg(delta)/60) avgdelta, round(max(delta)/60) maxdelta
+    $sql = <<<'EOF'
+select t.house, t.lastupdate,
+	t.mindelta, modes.delta as modedelta, t.avgdelta, t.maxdelta,
+	r.region, group_concat(r.name order by 1 separator ', ') nms
 from (
-select sn.updated,
-if(@prevhouse = sn.house and sn.updated > timestampadd(hour, -72, now()), unix_timestamp(sn.updated) - @prevdate, null) delta,
-@prevdate := unix_timestamp(sn.updated) updated_ts,
-@prevhouse := sn.house house
-from (select @prevhouse := null, @prevdate := null) setup, tblSnapshot sn
-order by sn.house, sn.updated) deltas
-group by deltas.house) t
+	select deltas.house, max(deltas.updated) lastupdate, round(min(delta)/5)*5 mindelta, round(avg(delta)/5)*5 avgdelta, round(max(delta)/5)*5 maxdelta
+	from (
+		select sn.updated,
+			if(@prevhouse = sn.house and sn.updated > timestampadd(hour, -48, now()), unix_timestamp(sn.updated) - @prevdate, null) delta,
+			@prevdate := unix_timestamp(sn.updated) updated_ts,
+			@prevhouse := sn.house house
+		from (select @prevhouse := null, @prevdate := null) setup, tblSnapshot sn
+		order by sn.house, sn.updated) deltas
+	group by deltas.house) t
+left join (
+	select house, delta
+	from (
+		select if(@prev = house, @rownum := @rownum + 1, @rownum := 0) o, delta, (@prev := house) as house, c
+		from (
+			select house, delta, count(*) c
+			from (
+				select sn.updated,
+					round(if(@prevh = sn.house, unix_timestamp(sn.updated) - @prevd, null)/5)*5 delta,
+					@prevd := unix_timestamp(sn.updated) updated_ts,
+					@prevh := sn.house house
+				from (select @prevh := null, @prevd := null) setup, tblSnapshot sn
+				where sn.updated > timestampadd(hour, -48, now())
+				order by sn.house, sn.updated) deltas
+			where delta is not null
+			group by house, delta
+			order by house, c desc
+		) tosort,
+		(select @rownum := 0, @prev := null) setup) filtered
+		where o=0
+	) modes on modes.house = t.house
 join tblRealm r on r.house = t.house and r.locale is not null
 group by r.house
-order by 3 asc, 4 asc, region asc, nms asc
+order by 4 asc, 3 asc, region asc, nms asc
 EOF;
 
     $stmt = $db->prepare($sql);
@@ -71,6 +90,7 @@ function BuildDataIntervalsTable(&$rows)
     <th>Region</th>
     <th>Realms</th>
     <th>Minimum Delay</th>
+    <th>Usual Delay</th>
     <th>Average Delay</th>
     <th>Maximum Delay</th>
     <th>Last Update</th>
@@ -85,6 +105,7 @@ EOF;
         $tr .= '<td>'.$row['region'].'</td>';
         $tr .= '<td>'.$row['nms'].'</td>';
         $tr .= '<td align="right">'.(is_null($row['mindelta']) ? '' : TimeDiff(time() - $row['mindelta'], $opt)).'</td>';
+        $tr .= '<td align="right">'.(is_null($row['modedelta']) ? '' : TimeDiff(time() - $row['modedelta'], $opt)).'</td>';
         $tr .= '<td align="right">'.(is_null($row['avgdelta']) ? '' : TimeDiff(time() - $row['avgdelta'], $opt)).'</td>';
         $tr .= '<td align="right">'.(is_null($row['maxdelta']) ? '' : TimeDiff(time() - $row['maxdelta'], $opt)).'</td>';
         $tr .= '<td align="right">'.TimeDiff(strtotime($row['lastupdate'])).'</td>';
