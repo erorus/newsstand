8ed4463f8b67da8c0098e8d4cd8dee163685cc24
Erorus <github@theunderminejournal.com>
2016-09-08 14:13:45 -0400

    fix unusuals page

--- public/api/category.php
+++ public/api/category.php
@@ -1878,7 +1878,7 @@ function CategoryUnusualItemList($house, $unusualSql, $allowCrafted = 0)
         'where' => $unusualSql . $craftedSql . ' and s.bonusset=0',
         'joins' => 'join tblAuction a on a.house=s.house and a.item=i.id join tblAuctionRare ar on ar.house=a.house and ar.id=a.id',
         'colsUpper'        => 'g.median * pow(1.15,(@level - cast(i.level as signed))/15) globalmedian, min(ar.prevseen) `lastseenover`, min(ifnull(a.buy/a.quantity, a.bid/a.quantity) * pow(1.15,(@level - cast(i.level as signed))/15)) `priceover`',
-        'colsLowerOutside' => 'g.median * pow(1.15,(@level - cast(r2.baselevel as signed))/15) globalmedian, r2.lastseenover, r2.priceover * * pow(1.15,(@level - cast(r2.baselevel as signed))/15) priceover',
+        'colsLowerOutside' => 'g.median * pow(1.15,(@level - cast(r2.baselevel as signed))/15) globalmedian, r2.lastseenover, r2.priceover * pow(1.15,(@level - cast(r2.baselevel as signed))/15) priceover',
         'colsLowerInside'  => 'min(ar.prevseen) `lastseenover`, min(ifnull(a.buy/a.quantity, a.bid/a.quantity)) `priceover`',
         'outside' => 'lastseenover as lastseen, priceover as price',
     ];
