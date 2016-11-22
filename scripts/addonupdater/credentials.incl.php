046ae08e515c25545fad3d6d560e1f310aa69e32
Erorus <github@theunderminejournal.com>
2016-11-22 00:09:54 -0500

    remove credentials and revoke curseforge token, lern to git

--- scripts/addonupdater/credentials.incl.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-define('CURSEFORGE_API_TOKEN', '1321a412-3664-47fc-a55e-26dad7c3b3b9');
-define('CURSEFORGE_PROJECT_ID', 29546);
-
-define('WOWI_USERNAME', 'TheUndermineJournal');
-define('WOWI_PASSWORD', 'abc123');
