2455e9b786d78e6bdb91b5b0f3bcc0bad1a882b1
Erorus <github@theunderminejournal.com>
2017-07-14 10:12:51 -0400

    add client credentials flow support for bnet api

--- incl/battlenet.incl.php
+++ incl/battlenet.incl.php
@@ -1,7 +1,8 @@
 <?php
 
-require_once(__DIR__ . '/memcache.incl.php');
-require_once(__DIR__ . '/battlenet.credentials.php');
+require_once __DIR__ . '/memcache.incl.php';
+require_once __DIR__ . '/battlenet.credentials.php';
+require_once __DIR__ . '/NewsstandHTTP.incl.php';
 
 define('BATTLE_NET_REQUEST_LIMIT', 50); // per period
 define('BATTLE_NET_REQUEST_PERIOD', 1); // seconds
@@ -43,9 +44,91 @@ function GetBattleNetURL($region, $path)
 
         MCDelete($cacheKey . '_critical');
 
-        $pattern = ($region == 'cn') ? 'https://api.battlenet.com.%s/%s%sapikey=%s' : 'https://%s.api.battle.net/%s%sapikey=%s';
-        $finalUrl = sprintf($pattern, $region, $path, strpos($path, '?') !== false ? '&' : '?', BATTLE_NET_KEY);
+        if (substr($path, 0, 5) == 'data/' || substr($path, 0, 8) == 'profile/') {
+            // new data api, uses client creds
+
+            $qs = '';
+            $pos = strpos($path, '?');
+            if ($pos !== false) {
+                $qs = substr($path, $pos + 1);
+                $path = substr($path, 0, $pos);
+            }
+
+            parse_str($qs, $qsa);
+
+            if (!isset($qsa['namespace'])) {
+                $qsa['namespace'] = 'dynamic-' . $region;
+            }
+            if (!isset($qsa['locale'])) {
+                $qsa['locale'] = 'en_US';
+            }
+            $qsa['access_token'] = GetBattleNetClientCredentials($region);
+
+            $pattern = ($region == 'cn') ? 'https://api.battlenet.com.%s/%s?%s' : 'https://%s.api.battle.net/%s?%s';
+            $finalUrl = sprintf($pattern, $region, $path, http_build_query($qsa));
+        } else {
+            // old api, uses api key
+            $pattern = ($region == 'cn') ? 'https://api.battlenet.com.%s/%s%sapikey=%s' : 'https://%s.api.battle.net/%s%sapikey=%s';
+            $finalUrl = sprintf($pattern, $region, $path, strpos($path, '?') !== false ? '&' : '?', BATTLE_NET_KEY);
+        }
     }
 
     return $finalUrl ? $finalUrl : false;
+}
+
+function GetBattleNetClientCredentials($region)
+{
+    static $knownCreds = [];
+
+    $region = trim(strtolower($region));
+    $partition = $region == 'cn' ? 'cn' : 'us';
+
+    if (isset($knownCreds[$partition]) && ($knownCreds[$partition]['expire'] > time())) {
+        return $knownCreds[$partition]['token'];
+    }
+
+    $cacheKey = 'BattleNetClientCredentials-' . $partition;
+
+    $creds = MCGet($cacheKey);
+    if ($creds && $creds['expire'] > time()) {
+        $knownCreds[$partition] = $creds;
+        return $creds['token'];
+    }
+
+    $endpoint = ($partition == 'cn') ? 'https://www.battlenet.com.cn/oauth/token' : sprintf(BATTLE_NET_TOKEN_URI, 'us');
+
+    $json = \Newsstand\HTTP::Get(
+        sprintf('%s?grant_type=client_credentials&client_id=%s&client_secret=%s',
+            $endpoint, BATTLE_NET_KEY, BATTLE_NET_SECRET), ['Accept' => 'application/json']);
+
+    if (!$json) {
+        trigger_error('Could not get client credentials from ' . $endpoint, E_USER_ERROR);
+        return '';
+    }
+
+    $data = json_decode($json, true);
+    if (json_last_error() != JSON_ERROR_NONE) {
+        trigger_error('Invalid json (' . json_last_error_msg() . ') returned in client credentials from ' . $endpoint, E_USER_ERROR);
+        return '';
+    }
+
+    if (isset($data['error'])) {
+        trigger_error('Client credentials returned error ' . (isset($data['error_description']) ? $data['error_description'] : $data['error']) . ' from ' . $endpoint, E_USER_ERROR);
+        return '';
+    }
+
+    if (!isset($data['access_token'])) {
+        trigger_error('Client credentials had no access token from ' . $endpoint, E_USER_ERROR);
+        return '';
+    }
+
+    $creds = [
+        'token' => $data['access_token'],
+        'expire' => time() + (isset($data['expires_in']) ? $data['expires_in'] : 86400),
+    ];
+
+    $knownCreds[$partition] = $creds;
+    MCSet($cacheKey, $creds, $creds['expire']);
+
+    return $creds['token'];
 }
