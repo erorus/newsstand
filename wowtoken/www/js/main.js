e885363509492634a5e94a7969553d5029ec3ffe
Erorus <github@theunderminejournal.com>
2017-09-18 16:04:31 -0400

    check for redefined getComputedStyle

--- wowtoken/www/js/main.js
+++ wowtoken/www/js/main.js
@@ -430,6 +430,10 @@ var wowtoken = {
                 return fail();
             }
 
+            if ('prototype' in window.getComputedStyle) {
+                return fail();
+            }
+
             var s = window.getComputedStyle(divs[0]);
             if (s.display != 'block') {
                 return fail();
