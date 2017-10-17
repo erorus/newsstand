be8cc4289ad73178dc9ac0832a424eb0342778a2
Erorus <github@theunderminejournal.com>
2017-10-17 12:33:30 -0400

    Revert "add adversal ad to item page"
    This reverts commit 0c1e6a70dd785a69f64d79b8f3614b5c9801b20b.

--- public/js/item.js
+++ public/js/item.js
@@ -799,7 +799,7 @@ var TUJ_Item = function ()
             dest.appendChild(i);
         }
 
-        dest.appendChild(libtuj.Ads.Add('10145332', 'box', '336x280'));
+        dest.appendChild(libtuj.Ads.Add('9943194718', 'box'));
     }
 
     function GetItemNotificationsList(itemId, bonusSet, mainDiv)
