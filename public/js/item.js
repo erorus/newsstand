ca83dffd5b651977a9c7265ae4f9d2354674525e
Erorus <github@theunderminejournal.com>
2017-11-04 22:12:46 -0400

    i don't always test my code, but when i do, i test it in production

--- public/js/item.js
+++ public/js/item.js
@@ -124,7 +124,7 @@ var TUJ_Item = function ()
                 tuj.SetParams({page: 'item', id: '' + itemId});
                 return;
             }
-        } else if (dta.stats[level].baselevel == level) {
+        } else if (level && dta.stats[level].baselevel == level) {
             // the level specified in the URL is the base level, which makes it redundant. remove it.
             tuj.SetParams({page: 'item', id: '' + itemId});
             return;
