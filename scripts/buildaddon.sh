b31c42f339034659744f084463a2250ebe7dd737
Erorus <github@theunderminejournal.com>
2019-03-09 19:56:46 +0000

    use advzip to recompress the zip before submit

--- scripts/buildaddon.sh
+++ scripts/buildaddon.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 php /var/newsstand/scripts/buildaddon.php
 if [ $? -eq 0 ]; then
+    /var/newsstand/scripts/advzip -z -2 /var/newsstand/addon/TheUndermineJournal.zip
     /var/newsstand/scripts/addonupdater/addonupdater.sh
 fi
