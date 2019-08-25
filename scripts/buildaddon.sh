70581046423a9bcaa1b2a5ce82ac9e64fdcf2873
Erorus <editor@theunderminejournal.com>
2019-08-25 17:18:33 +0000

    use OS-installed advzip

--- scripts/buildaddon.sh
+++ scripts/buildaddon.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 php /var/newsstand/scripts/buildaddon.php
 if [ $? -eq 0 ]; then
-    /var/newsstand/scripts/advzip -z -2 /var/newsstand/addon/TheUndermineJournal.zip
+    advzip -z -2 /var/newsstand/addon/TheUndermineJournal.zip
+    chmod +r /var/newsstand/addon/TheUndermineJournal.zip
     /var/newsstand/scripts/addonupdater/addonupdater.sh
 fi
