3302158540808fced3a4f2882c97d819ea443abd
Erorus <github@theunderminejournal.com>
2021-04-04 17:01:02 +0000

    use level curve prio

--- incl/BonusItemLevel.incl.php
+++ incl/BonusItemLevel.incl.php
@@ -10,12 +10,12 @@ class BonusItemLevel
 
     public static function init($db) {
         static::$bonusCurveCache = [];
-        $stmt = $db->prepare('select id, levelcurve from tblDBCItemBonus where levelcurve is not null');
+        $stmt = $db->prepare('select id, levelcurve, levelcurvepriority from tblDBCItemBonus where levelcurve is not null');
         $stmt->execute();
-        $id = $curve = null;
-        $stmt->bind_result($id, $curve);
+        $id = $curve = $prio = null;
+        $stmt->bind_result($id, $curve, $prio);
         while ($stmt->fetch()) {
-            static::$bonusCurveCache[$id] = $curve;
+            static::$bonusCurveCache[$id] = [$curve, $prio];
         }
         $stmt->close();
 
@@ -43,11 +43,22 @@ class BonusItemLevel
     public static function GetBonusItemLevel($bonuses, $defaultItemLevel, $lootedLevel) {
         $levelSum = $defaultItemLevel;
 
+        $currentCurve = null;
+        $currentPrio = null;
+
         foreach ($bonuses as $bonus) {
             if (isset(static::$bonusCurveCache[$bonus])) {
-                return static::GetCurvePoint(static::$bonusCurveCache[$bonus], $lootedLevel);
+                list($curve, $prio) = static::$bonusCurveCache[$bonus];
+                if (is_null($currentPrio) || $currentPrio > $prio) {
+                    $currentCurve = $curve;
+                    $currentPrio = $prio;
+                }
             }
             $levelSum += isset(static::$bonusLevelCache[$bonus]) ? static::$bonusLevelCache[$bonus] : 0;
+        }
+
+        if (!is_null($currentCurve)) {
+            return static::GetCurvePoint($currentCurve, $lootedLevel);
         }
 
         return $levelSum;
