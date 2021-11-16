94fe0993e0585c4d101e70cf445513f47889c562
Erorus <github@theunderminejournal.com>
2021-11-16 14:49:01 -0500

    remove newswire

--- public/api/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-header('Location: https://newswire.theunderminejournal.com/');
--- scripts/backupdata.sh
+++ scripts/backupdata.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 find /var/newsstand/backup -name '*.sql.gz' -ctime +15 -delete
 php /var/newsstand/scripts/backupdata.php
-
-latest=`find /var/newsstand/backup -name 'backupdata.*.sql.gz' -ctime -2 | sort | tail -n 1`
-if [ "$latest" != "" ]; then
-    source /var/newsstand/scripts/newswire.credentials.sh
-
-    echo "`date` Turning off newswire sql"
-    ssh newswire ./sqloff.sh
-    echo "`date` Updating newswire sql"
-    cat "$latest" | ssh newswire 'zcat | mysql -u root -p'\'"$NEWSWIREMYSQL"\'' newsstand'
-    echo "`date` Turning on newswire sql"
-    ssh newswire ./sqlon.sh
-    echo "`date` Done with newswire"
-fi
